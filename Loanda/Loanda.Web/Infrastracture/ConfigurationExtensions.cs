130d110a1d5ed97a04014050accd32b1b20597c4
mimski <maria.bg.1989@gmail.com>
2019-11-07 20:50:30 +0200

    Add ConfigureExtensions to extract default connection string, clean up usings

---
+++ Loanda/Loanda.Web/Infrastracture/ConfigurationExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Loanda.Web.Infrastracture
+{
+    public static class ConfigurationExtensions
+    {
+        public static string GetDefaultConnectionString(this IConfiguration configuration)
+            => configuration.GetConnectionString("DefaultConnection");
+    }
+}
