0ecd97a3b703fdefd1dc77c944030dc0eae34bd8
mimski <maria.bg.1989@gmail.com>
2019-11-07 14:39:50 +0200

    Create EmailStatusConfiguration.cs
    Add EmailStatusConfiguration

---
+++ Loanda/Loanda.Data/Context/EntityConfigurations/EmailStatusConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Loanda.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Loanda.Data.Context.EntityConfigurations
+{
+    internal class EmailStatusConfiguration : IEntityTypeConfiguration<EmailStatus>
+    {
+        public void Configure(EntityTypeBuilder<EmailStatus> builder)
+        {
+            builder.ToTable("EmailStatuses");
+
+            builder.HasKey(emailStatuses => emailStatuses.Id);
+
+            builder.Property(emailStatuses => emailStatuses.Name)
+                .HasMaxLength(20)
+                .IsRequired();
+        }
+    }
+}
