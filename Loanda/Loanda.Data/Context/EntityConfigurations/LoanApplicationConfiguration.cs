7e4954c20cc80f70a05554f1e9eb38fa448e7839
mimski <maria.bg.1989@gmail.com>
2019-11-07 14:40:06 +0200

    Create LoanApplicationConfiguration.cs
    Add LoanApplicationConfiguration

---
+++ Loanda/Loanda.Data/Context/EntityConfigurations/LoanApplicationConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Loanda.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Loanda.Data.Context.EntityConfigurations
+{
+    internal class LoanApplicationConfiguration : IEntityTypeConfiguration<LoanApplication>
+    {
+        public void Configure(EntityTypeBuilder<LoanApplication> builder)
+        {
+            builder.ToTable("LoanApplications");
+
+            builder.HasKey(loanApplication => loanApplication.Id);
+
+            builder.Property(loanApplication => loanApplication.CreatedOn)
+                .HasColumnType("date")
+              .IsRequired(false);
+
+            builder.Property(loanApplication => loanApplication.ModifiedOn)
+                 .HasColumnType("date")
+               .IsRequired(false);
+
+            builder.Property(loanApplication => loanApplication.DeletedOn)
+                 .HasColumnType("date")
+               .IsRequired(false);
+
+            builder.Property(loanApplication => loanApplication.IsDeleted)
+                .HasDefaultValue(false);
+        }
+    }
+}
