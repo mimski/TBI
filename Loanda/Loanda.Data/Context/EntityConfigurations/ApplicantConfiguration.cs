799eee6ef237d8385b4f6cefa5fc215bc815557a
mimski <maria.bg.1989@gmail.com>
2019-11-07 14:38:30 +0200

    Create ApplicantConfiguration.cs
    Add ApplicantConfiguration

---
+++ Loanda/Loanda.Data/Context/EntityConfigurations/ApplicantConfiguration.cs
@@ -0,0 +1,64 @@
+﻿using Loanda.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Loanda.Data.Context.EntityConfigurations
+{
+    internal class ApplicantConfiguration : IEntityTypeConfiguration<Applicant>
+    {
+        public void Configure(EntityTypeBuilder<Applicant> builder)
+        {
+            builder.ToTable("Applicants");
+
+            builder.HasKey(applicant => applicant.Id);
+
+            builder.Property(applicant => applicant.EGN)
+               .IsRequired();
+
+            builder.Property(applicant => applicant.FirstName)
+                .HasMaxLength(120)
+                .IsRequired();
+
+            builder.Property(applicant => applicant.MiddleName)
+                .HasMaxLength(120)
+                .IsRequired(false);
+
+            builder.Property(applicant => applicant.LastName)
+                .HasMaxLength(120)
+                .IsRequired();
+
+            builder.Property(applicant => applicant.DateOfBirth)
+                .HasColumnType("date")
+                .IsRequired(false);
+
+            builder.Property(applicant => applicant.PhoneNumber)
+               .IsRequired();
+
+            builder.Property(applicant => applicant.Address)
+              .HasMaxLength(1200)
+              .IsRequired();
+
+            builder.Property(applicant => applicant.City)
+               .HasMaxLength(40)
+               .IsRequired();
+
+            builder.Property(applicant => applicant.EmailAddress)
+                .IsRequired();
+
+            builder.Property(applicant => applicant.CreatedOn)
+                 .HasColumnType("date")
+               .IsRequired(false);
+
+            builder.Property(applicant => applicant.ModifiedOn)
+                 .HasColumnType("date")
+               .IsRequired(false);
+
+            builder.Property(applicant => applicant.DeletedOn)
+                 .HasColumnType("date")
+               .IsRequired(false);
+
+            builder.Property(applicant => applicant.IsDeleted)
+                .HasDefaultValue(false);
+        }
+    }
+}
