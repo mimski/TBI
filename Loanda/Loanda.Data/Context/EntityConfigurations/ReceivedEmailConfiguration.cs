e4ff0f4911381643888d6e6a26c52970613eb55f
mimski <maria.bg.1989@gmail.com>
2019-11-07 14:40:23 +0200

    Create ReceivedEmailConfiguration.cs
    Add ReceivedEmailConfiuration

---
+++ Loanda/Loanda.Data/Context/EntityConfigurations/ReceivedEmailConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Loanda.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Loanda.Data.Context.EntityConfigurations
+{
+    internal class ReceivedEmailConfiguration : IEntityTypeConfiguration<ReceivedEmail>
+    {
+        public void Configure(EntityTypeBuilder<ReceivedEmail> builder)
+        {
+            builder.ToTable("ReceivedEmails");
+
+            builder.HasKey(receivedEmail => receivedEmail.Id);
+
+            builder.Property(receivedEmail => receivedEmail.IsReviewed)
+                .HasDefaultValue(false);
+
+            builder.Property(loanApplication => loanApplication.CreatedOn)
+              .HasColumnType("date")
+            .IsRequired(false);
+
+            builder.Property(loanApplication => loanApplication.ModifiedOn)
+                 .HasColumnType("date")
+               .IsRequired(false);
+
+            builder.Property(loanApplication => loanApplication.DeletedOn)
+                 .HasColumnType("date")
+               .IsRequired(false);
+
+            builder.Property(loanApplication => loanApplication.IsDeleted)
+                .HasDefaultValue(false);
+        }
+    }
+}
