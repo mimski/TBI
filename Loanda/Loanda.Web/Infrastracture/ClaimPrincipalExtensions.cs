38ba8081000cb861efa5e59a92a6ccca26494c4f
mimski <maria.bg.1989@gmail.com>
2019-11-08 18:26:05 +0200

    Add ClaimsPrincipalExtensions.cs with extension to Get the user id

---
+++ Loanda/Loanda.Web/Infrastracture/ClaimPrincipalExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace Loanda.Web.Infrastracture
+{
+    public static class ClaimPrincipalExtensions
+    {
+        public static string GetUserId(this ClaimsPrincipal claimsPrincipal)
+        => claimsPrincipal.FindFirstValue(ClaimTypes.NameIdentifier);
+    }
+}
