3f98d73b0463f1b6b24dfe6789fc3ee8592b1276
mimski <maria.bg.1989@gmail.com>
2019-11-08 18:18:16 +0200

    Add EmailService

---
+++ Loanda/Loanda.Services/EmailService.cs
@@ -0,0 +1,44 @@
+﻿using Loanda.Data.Context;
+using Loanda.Entities;
+using Loanda.Services.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loanda.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly LoandaContext context;
+
+        public EmailService(LoandaContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Guid> Create(string senderEmail, DateTime DateReceived, string subject, string body)
+        {
+            var email = new ReceivedEmail
+            {
+                SenderEmail = senderEmail,
+                DateReceived = DateReceived,
+                Subject = subject,
+                Body = body
+            };
+
+            this.context.Add(email);
+
+            await this.context.SaveChangesAsync();
+
+            return email.Id;
+        }
+
+        //public async Task<IEnumerable<EmailService>> GetAll()
+        //{
+        //    return await this.context.ReceivedEmails.ToListAsync();  
+        //}
+
+    }
+}
