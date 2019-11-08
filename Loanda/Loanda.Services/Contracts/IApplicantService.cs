d8b36d55a5bc623af39ead54394e0b0915a252ec
mimski <maria.bg.1989@gmail.com>
2019-11-08 19:10:55 +0200

    Add ApplicantService - Create, Edit and Exists methods

---
+++ Loanda/Loanda.Services/Contracts/IApplicantService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loanda.Services.Contracts
+{
+    public interface IApplicantService
+    {
+        Task<Guid> Create(string firstName, string middleName, string lastName, string egn, string phoneNumber, string adress, string city);
+
+        Task<bool> Edit(Guid id, string firstName, string middleName, string lastName, string egn, string phoneNumber, string adress, string city);
+
+        Task<bool> Exists(int id);
+    }
+}
