6e58411a17629b9d5a7ab2436b5460eab266801e
mimski <maria.bg.1989@gmail.com>
2019-11-11 20:35:25 +0200

    Add override for SaveChanges with Audition and Deletion Rules

--- Loanda/Loanda.Data/Context/LoandaContext.cs
+++ Loanda/Loanda.Data/Context/LoandaContext.cs
@@ -1,9 +1,13 @@
 ﻿using Loanda.Data.JsonTools;
 using Loanda.Entities;
+using Loanda.Entities.Base.Contracts;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
 
 namespace Loanda.Data.Context
 {
@@ -19,8 +23,11 @@ namespace Loanda.Data.Context
         public virtual DbSet<LoanApplication> LoanApplications { get; set; }
 
         public virtual DbSet<ReceivedEmail> ReceivedEmails { get; set; }
+
         public virtual DbSet<ApplicationStatus> ApplicationStatuses { get; set; }
+
         public virtual DbSet<EmailAttachment> EmailAttachments { get; set; }
+
         public virtual DbSet<EmailStatus> EmailStatuses { get; set; }
 
         private void LoadJsonDataInDatabase(ModelBuilder modelBuilder)
@@ -46,6 +53,60 @@ namespace Loanda.Data.Context
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
             base.OnModelCreating(modelBuilder);
+        }
+
+        public override int SaveChanges()
+        {
+            this.ApplyDeletionRules();
+            this.ApplyAuditInfoRules();
+            return base.SaveChanges();
+        }
+
+        public virtual async Task<int> SaveChangesAsync(bool applyDeletionRules = true, bool applyAuditInfoRules = true)
+        {
+            if (applyDeletionRules is true)
+            {
+                this.ApplyDeletionRules();
+            }
+
+            if (applyAuditInfoRules is true)
+            {
+                this.ApplyAuditInfoRules();
+            }
+            return await base.SaveChangesAsync();
+        }
+
+        private void ApplyDeletionRules()
+        {
+            var entitiesForDeletion = this.ChangeTracker.Entries()
+                .Where(entity => entity.State is EntityState.Deleted && entity is IDeletable);
+
+            foreach (var entity in entitiesForDeletion)
+            {
+                var entityForDeletion = (IDeletable)entity.Entity;
+                entityForDeletion.DeletedOn = DateTime.UtcNow.AddHours(2);
+                entityForDeletion.IsDeleted = true;
+                entity.State = EntityState.Modified;
+            }
+        }
+
+        private void ApplyAuditInfoRules()
+        {
+            var newEntities = this.ChangeTracker.Entries()
+                .Where(entity => entity.Entity is IAuditable && (entity.State is EntityState.Added || entity.State is EntityState.Modified));
+
+            foreach (var entity in newEntities)
+            {
+                var newEntity = (IAuditable)entity.Entity;
+                if (entity.State is EntityState.Added && newEntity.CreatedOn is null)
+                {
+                    newEntity.CreatedOn = DateTime.UtcNow.AddHours(2);
+                }
+                else
+                {
+                    newEntity.ModifiedOn = DateTime.UtcNow.AddHours(2);
+                }
+            }
         }
     }
 }
