b1e945f32a91c5b81dad2314bca64f1a98419002
mimski <maria.bg.1989@gmail.com>
2019-11-12 17:50:06 +0200

    Add EmailAttachmentDtoMapper

---
+++ Loanda/Loanda.Services/Mapper/Contracts/IEmailDtoMapper.cs
@@ -0,0 +1,14 @@
+﻿using Loanda.Entities;
+using Loanda.Services.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loanda.Services.Mapper.Contracts
+{
+    public interface IEmailDtoMapper
+    {
+        EmailDTO Map(ReceivedEmail recievedEmail);
+        ReceivedEmail Map(EmailDTO recievedEmail);
+    }
+}
