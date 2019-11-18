39b421cf34e5d7670dbcdf2c7a8d91f44bc33fb0
mimski <maria.bg.1989@gmail.com>
2019-11-18 18:13:53 +0200

    Update EmailAttachmentDTO.cs

--- Loanda/Loanda.Services/DTOs/EmailAttachmentDTO.cs
+++ Loanda/Loanda.Services/DTOs/EmailAttachmentDTO.cs
@@ -6,7 +6,7 @@ namespace Loanda.Services.DTOs
 {
     public class EmailAttachmentDTO
     {
-        public long FileSizeInMb { get; set; }
+        public double? FileSizeInMb { get; set; }
 
         public string Content { get; set; }
     }
