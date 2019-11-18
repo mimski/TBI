cd37dd9eab313c8c711f2854615f62ddc5a30e51
mimski <maria.bg.1989@gmail.com>
2019-11-18 18:14:06 +0200

    Update IApplicantService.cs

--- Loanda/Loanda.Services/Contracts/IApplicantService.cs
+++ Loanda/Loanda.Services/Contracts/IApplicantService.cs
@@ -1,16 +1,30 @@
-﻿using System;
+﻿using Loanda.Services.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Loanda.Services.Contracts
 {
     public interface IApplicantService
     {
-        Task<Guid> CreateAsync(string firstName, string middleName, string lastName, string egn, string phoneNumber, string adress, string city);
+        //Task<Guid> CreateAsync(string firstName, string middleName, string lastName, string egn, string phoneNumber, string adress, string city);
 
-        Task<bool> EditAsync(Guid id, string firstName, string middleName, string lastName, string egn, string phoneNumber, string adress, string city);
+        //Task<bool> EditAsync(Guid id, string firstName, string middleName, string lastName, string egn, string phoneNumber, string adress, string city);
 
-        Task<bool> ExistsAsync(Guid id);
+        //Task<bool> ExistsAsync(Guid id);
+
+        Task<Applicant> AddAsync(Applicant applicant, CancellationToken ct);
+
+        Task<IReadOnlyCollection<Applicant>> GetAllAsync(CancellationToken ct);
+
+        Task<Applicant> GetByIdAsync(Guid id, CancellationToken ct);
+
+        Task<Applicant> UpdateAsync(Applicant applicant, CancellationToken ct);
+
+        Task<Applicant> MarkAsDeletedAsync(Guid id, CancellationToken ct);
+
+        Task RemoveAsync(Guid id, CancellationToken ct);
     }
 }
