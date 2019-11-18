4b4c60d3128faa278a741f14539f726035e8788b
mimski <maria.bg.1989@gmail.com>
2019-11-19 01:43:53 +0200

    Add ApplicantViewModel

---
+++ Loanda/Loanda.Web/Models/Applicant/ApplicantViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Loanda.Web.Models.Applicant
+{
+    public class ApplicantViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string EGN { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public DateTime? CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public DateTime? DeleteOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
