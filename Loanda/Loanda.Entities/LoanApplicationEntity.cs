abe168cb717588bc7cc144e2b32d2e0a24dc89c0
mimski <maria.bg.1989@gmail.com>
2019-11-18 18:12:30 +0200

    Updated LoanApplication to LoanApplicationEntity

---
+++ Loanda/Loanda.Entities/LoanApplicationEntity.cs
@@ -0,0 +1,20 @@
+﻿using Loanda.Entities.Base;
+using System;
+
+namespace Loanda.Entities
+{
+    public class LoanApplicationEntity : BaseEntity
+    {
+        public virtual ApplicantEntity Applicant { get; set; }
+
+        public Guid ApplicantId { get; set; }
+
+        public virtual ApplicationStatusEntity ApplicationStatus { get; set; }
+
+        public int ApplicationStatusId { get; set; }
+
+        public decimal LoanAmount { get; set; }
+
+        public bool? IsApproved { get; set; }
+    }
+}
