dc5390df28f05d02e722ec2f02a5cb404b721955
mimski <maria.bg.1989@gmail.com>
2019-11-18 18:13:13 +0200

    Add LoanApplicationViewModel

---
+++ Loanda/Loanda.Web/Models/LoanApplication/LoanApplicationViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loanda.Web.Models.LoanApplication
+{
+    public class LoanApplicationViewModel
+    {
+        public Guid Id { get; set; }
+
+        public decimal LoanAmount { get; set; }
+
+        public Guid ApplicantId { get; set; }
+
+        public int ApplicationStatusId { get; set; }
+
+    }
+}
