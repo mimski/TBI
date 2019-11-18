4e06ccdbfbe0aa7589a5543b4aefcd9ea07a1375
mimski <maria.bg.1989@gmail.com>
2019-11-19 01:48:38 +0200

    Add Create method in ApplicantController

--- Loanda/Loanda.Web/Controllers/ApplicantController.cs
+++ Loanda/Loanda.Web/Controllers/ApplicantController.cs
@@ -1,16 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Loanda.Services.Contracts;
+using Loanda.Web.Mappings;
+using Loanda.Web.Models.Applicant;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Loanda.Web.Controllers
 {
+    [Authorize]
     public class ApplicantController : Controller
     {
+        private readonly IApplicantService applicantService;
+
+        public ApplicantController(IApplicantService applicantService)
+        {
+            this.applicantService = applicantService ?? throw new ArgumentNullException(nameof(applicantService));
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [TempData]
+        public string ErrorMessage { get; set; }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            //var bookViewModel = new BookViewModel();
+
+            //return View(bookViewModel);
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ApplicantViewModel applicantViewModel, CancellationToken cancellationToken)
+        {
+            //if (!this.ModelState.IsValid)
+            //{
+            //    //throw new ArgumentException("Invalid input!");
+            //    return this.View();
+            //}
+
+            await this.applicantService.AddAsync(applicantViewModel.ToServiceModel(), cancellationToken);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
