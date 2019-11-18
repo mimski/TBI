6ec64f60ffed574f9258a94345b84eb1434d8122
mimski <maria.bg.1989@gmail.com>
2019-11-18 18:14:41 +0200

    Add LoanApplicationService

---
+++ Loanda/Loanda.Services/Contracts/ILoanApplicationService.cs
@@ -0,0 +1,24 @@
+﻿using Loanda.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Loanda.Services.Contracts
+{
+    public interface ILoanApplicationService
+    {
+        Task<IReadOnlyCollection<LoanApplication>> GetAllAsync(CancellationToken ct);
+
+        Task<LoanApplication> GetByIdAsync(Guid id, CancellationToken ct);
+
+        Task<LoanApplication> UpdateAsync(LoanApplication loanApplication, CancellationToken ct);
+
+        Task<LoanApplication> AddAsync(LoanApplication loanApplication, CancellationToken ct);
+
+        Task<LoanApplication> MarkAsDeletedAsync(Guid id, CancellationToken ct);
+
+        Task RemoveAsync(Guid id, CancellationToken ct);
+    }
+}
---
+++ Loanda/Loanda.Services/LoanApplicationService.cs
@@ -0,0 +1,87 @@
+﻿using Loanda.Data.Context;
+using Loanda.Services.Contracts;
+using Loanda.Services.Mappings;
+using Loanda.Services.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Loanda.Services
+{
+    public class LoanApplicationService : ILoanApplicationService
+    {
+        private readonly LoandaContext context;
+
+        public LoanApplicationService(LoandaContext context)
+        {
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<LoanApplication> AddAsync(LoanApplication loanApplication, CancellationToken ct)
+        {
+            var addedLoanApplicationEntry = this.context.LoanApplications.Add(loanApplication.ToEntity());
+            await this.context.SaveChangesAsync(ct);
+            return addedLoanApplicationEntry.Entity.ToService();
+        }
+
+        public async Task<IReadOnlyCollection<LoanApplication>> GetAllAsync(CancellationToken ct)
+        {
+            var loanApplications = await this.context.LoanApplications.AsNoTracking().OrderByDescending(application => application.LoanAmount).ToListAsync(ct);
+            return loanApplications.ToService();
+        }
+
+        public async Task<LoanApplication> GetByIdAsync(Guid id, CancellationToken ct)
+        {
+            var loanApplication = await this.context.LoanApplications.AsNoTracking().SingleOrDefaultAsync(application => application.Id.Equals(id), ct);
+            return loanApplication.ToService();
+        }
+
+        //public async Task<LoanApplication> UpdateAsync(LoanApplication loanApplication, CancellationToken ct)
+        //{
+        //    var updatedLoanApplicationEntry = this.context.LoanApplications.Update(loanApplication.ToEntity());
+        //    await this.context.SaveChangesAsync(ct);
+        //    return updatedLoanApplicationEntry.Entity.ToService();
+        //}
+
+        public async Task<LoanApplication> UpdateAsync(LoanApplication loanApplication, CancellationToken ct)
+        {
+            var existingLoanApplication = await this.context.LoanApplications.SingleOrDefaultAsync(application => application.Id.Equals(loanApplication.Id), ct);
+            this.context.Entry(existingLoanApplication).CurrentValues.SetValues(loanApplication.ToEntity());
+            await this.context.SaveChangesAsync();
+
+            return existingLoanApplication.ToService();
+        }
+
+        public async Task<LoanApplication> MarkAsDeletedAsync(Guid id, CancellationToken ct)
+        {
+            var loanApplication = await this.context.LoanApplications.SingleOrDefaultAsync(application => application.Id.Equals(id), ct);
+            if (loanApplication != null)
+            {
+                loanApplication.IsDeleted = true;
+                loanApplication.DeletedOn = DateTime.UtcNow.AddHours(2);
+
+                this.context.LoanApplications.Update(loanApplication);
+                await this.context.SaveChangesAsync(ct);
+                return loanApplication.ToService();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public async Task RemoveAsync(Guid id, CancellationToken ct)
+        {
+            var loanApplication = await this.context.LoanApplications.SingleOrDefaultAsync(application => application.Id.Equals(id), ct);
+            if (loanApplication != null)
+            {
+                this.context.Remove(loanApplication);
+                await this.context.SaveChangesAsync(ct);
+            }
+        }
+    }
+}
