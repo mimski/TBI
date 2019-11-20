4c53b4d19ab69fc5f6b9d348bc03addc22d7cf43
mimski <maria.bg.1989@gmail.com>
2019-11-20 22:01:08 +0200

    Update ReceivedEmail.cs

--- Loanda/Loanda.Services/Models/ReceivedEmail.cs
+++ Loanda/Loanda.Services/Models/ReceivedEmail.cs
@@ -1,35 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using Loanda.Entities;
 
 namespace Loanda.Services.Models
 {
     public class ReceivedEmail
     {
-        public Guid Id { get; set; }
+        public long Id { get; set; }
 
         public string SenderEmail { get; set; }
 
         public string SenderName { get; set; }
 
-        public DateTime DateReceived { get; set; }
+        public string DateReceived { get; set; }
 
         public string Subject { get; set; }
 
         public string Body { get; set; }
 
-        //public virtual ICollection<EmailAttachmentEntity> EmailAttachments { get; set; }
+        public virtual ICollection<EmailAttachmentEntity> EmailAttachments { get; set; }
 
-        //public virtual EmailStatusEntity EmailStatus { get; set; }
+        public virtual EmailStatusEntity EmailStatus { get; set; }
 
         public int? EmailStatusId { get; set; }
 
-        //public virtual ApplicantEntity Applicant { get; set; }
+        public virtual ApplicantEntity Applicant { get; set; }
 
         public Guid? ApplicantId { get; set; }
 
-        public bool IsReviewed { get; set; }
-
         public string GmailEmailId { get; set; }
+
+        public DateTime? CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
     }
 }
