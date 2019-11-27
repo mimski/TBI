fe35aa57afa6598375dbc0d7e1953e8da781e590
Dimarata <ivanov_dimo@abv.bg>
2019-11-28 00:25:56 +0200

    Add IUserService

---
+++ Loanda/Loanda.Services/Contracts/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loanda.Services.Contracts
+{
+    public interface IUserService
+    {
+        Task<bool> CheckForEmail(string email);
+        Task<bool> CheckForUserName(string username);
+    }
+}
