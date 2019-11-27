656a8ee0bb84c58787ef08a5d4ef2058fdfd2f95
Dimarata <ivanov_dimo@abv.bg>
2019-11-28 00:28:32 +0200

    Add ChangePasswordViewModel

---
+++ Loanda/Loanda.Web/Models/ChangePasswordViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loanda.Web.Models
+{
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+    }
+}
