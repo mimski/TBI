8764addd4d1561e7cc4ffee700f267d9e23ddb95
Dimarata <ivanov_dimo@abv.bg>
2019-11-28 00:26:08 +0200

    Add UserService

---
+++ Loanda/Loanda.Services/UserService.cs
@@ -0,0 +1,38 @@
+﻿using Loanda.Data.Context;
+using Loanda.Services.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loanda.Services
+{
+   public class UserService : IUserService
+    {
+        private readonly LoandaContext dbcontext;
+        public UserService(LoandaContext dbcontext)
+        {
+            this.dbcontext = dbcontext;
+        }
+
+        public async Task<bool> CheckForEmail(string email)
+        {
+            if (await this.dbcontext.Users.AnyAsync(x => x.Email == email))
+            {
+                return true;
+            }
+
+            return false;
+        }
+        public async Task<bool> CheckForUserName(string username)
+        {
+            if (await this.dbcontext.Users.AnyAsync(x => x.UserName == username))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
